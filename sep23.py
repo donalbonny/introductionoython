d5467ca229237923b9aa10e1ac483ee7299450c7
Vinh Pham <phamngocvinh1988@gmail.com>
2016-10-09 22:57:08 -0500

    Add files via upload

---
+++ sep23.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+get_ipython().magic('pwd ')
+get_ipython().magic('ls ')
+UnigeneList= []
+get_ipython().magic('pinfo list.append')
+get_ipython().magic('cd ')
+with open('home/vinhpham/reformatted_Unigene.fa', 'r') as inFile:
+    UnigeneStr= inFile.read()
+    
+get_ipython().magic('cd ')
+get_ipython().magic('ls ')
+with open('/home/vinhpham/reformatted_Unigene.fa', 'r') as inFile:
+    UnigeneStr= inFile.read()
+    
+get_ipython().magic('whos')
+with open('/home/vinhpham/reformatted_Unigene.fa', 'r') as inFile:
+    UnigeneStr= inFile.read().splitlines()
+    
+    
+get_ipython().magic('whos')
+UnigeneStr[0]
+with open('/home/vinhpham/reformatted_Unigene.fa', 'r') as inFile:
+    UnigeneStr= inFile.read().splitlines()
+    for record in UnigeneStr:
+        UnigeneList.append(record.split('')) 
+        
+    
+with open('/home/vinhpham/reformatted_Unigene.fa', 'r') as inFile:
+    UnigeneStr= inFile.read().splitlines()
+    for record in UnigeneStr:
+        UnigeneList.append(record.split(' '))
+         
+        
+    
+get_ipython().magic('whos')
+for record in UnigeneList:
+    print(len(record[6]))
+    
+get_ipython().magic('whos')
+get_ipython().magic('save Oct-8.py')
+get_ipython().magic('pinfo vi')
+get_ipython().magic('pinfo save')
+get_ipython().magic('save Oct-9.py')
+get_ipython().magic('save Ocr-9')
+get_ipython().magic('pinfo save')
+get_ipython().magic('save Oct09')
+get_ipython().magic('save Oct-09.py')
+get_ipython().magic('pinfo save')
+get_ipython().magic('save python Oct-9')
+$save python Oct09
+get_ipython().magic('save python Oct09')
+get_ipython().magic('save python n1')
+get_ipython().magic('save python')
+get_ipython().magic('save [-r] SEP')
+get_ipython().magic('pinfo save')
+get_ipython().magic('save [-a] SEP')
+get_ipython().magic('save [-r] SEP')
+get_ipython().magic('cd ')
+get_ipython().magic('ls ')
+get_ipython().magic('save [-a] sep23.py')
+get_ipython().magic('save [-a] sep23 1-40')
